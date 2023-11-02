e22494d57aed9878fa116e6ac8ce99aa48be332b
Amol Mawale <amol@99ideassaas.com>
2023-11-03 00:47:59 +0530

    Connected to Database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
